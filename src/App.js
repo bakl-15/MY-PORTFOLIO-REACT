facfc4af76d3fd956c327b87244f43b0b78bcf4b
bakl <you@example.com>
2024-12-01 23:07:58 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Button from './components/form/Button';
+function App() {
+  return (
+    <div className="App">
+      <h1>sofiane</h1>
+      <Button primary >Je suis loin de</Button>
+    </div>
+  );
+}
+
+export default App;
